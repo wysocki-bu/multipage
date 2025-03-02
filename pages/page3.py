5fe25f0268063c6c67f54ac39b3f5722daea32f6
wysocki-bu <wysockip@bu.edu>
2025-03-02 11:21:19 -0500

    Update page3.py

--- pages/page3.py
+++ pages/page3.py
@@ -4,15 +4,7 @@ st.title("Page 3")
 
 if 'data' in st.session_state:
    st.write("Closing Price Over Time:")
-   dd = st.session_state.data
-   st.line_chart(dd['Close'])
+   st.line_chart(data['Close'])
 else:
    st.write("No DataFrame found. Please create it in the 'Create DataFrame' page.")
 
-#    if "Close" in data:
-#        st.subheader("Closing Price Over Time")
-#        st.line_chart(data['Close'])
-#    else:
-#        st.warning("Closing price data is not available for this stock.")
-
-#st.session_state.data = st.data_editor(st.session_state.data)
