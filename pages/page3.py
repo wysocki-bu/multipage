94d9d658879bca7460e1a95773a525767be9c109
wysocki-bu <wysockip@bu.edu>
2025-03-02 11:05:50 -0500

    Update page3.py

--- pages/page3.py
+++ pages/page3.py
@@ -1,3 +1,15 @@
 import streamlit as st
 
 st.title("Page 3")
+
+if 'data' in st.session_state:
+   st.write("Closing Price Over Time:")
+   st.write(st.session_state.data.describe())
+else:
+   st.write("No DataFrame found. Please create it in the 'Create DataFrame' page.")
+
+#    if "Close" in data:
+#        st.subheader("Closing Price Over Time")
+#        st.line_chart(data['Close'])
+#    else:
+#        st.warning("Closing price data is not available for this stock.")
