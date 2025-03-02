dff7df3d5c6834957901408728d8b9870f6e2780
wysocki-bu <wysockip@bu.edu>
2025-03-02 10:54:22 -0500

    Update page1.py

--- pages/page1.py
+++ pages/page1.py
@@ -13,12 +13,11 @@ end_date = st.sidebar.date_input("End Date", value=pd.to_datetime("2024-12-31"))
 
 # Fetching data
 st.write(f"Data for **{ticker_symbol}** from {start_date} to {end_date}...")
-if 'data' not in st.session_state:
+#if 'data' not in st.session_state:
     df = yf.download(ticker_symbol, start=start_date, end=end_date)
     if df.empty:
        st.error("No data found. Please check the ticker symbol or date range.")
        st.stop()
     st.session_state.data = df
-st.write(f"Fetching data for **{ticker_symbol}** from {start_date} to {end_date}...")
 
 st.session_state.df = st.data_editor(st.session_state.data)
