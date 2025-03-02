090b591ac18e1efbf9dd6ac62acbf4525615e4e4
wysocki-bu <wysockip@bu.edu>
2025-03-02 10:36:26 -0500

    Update page2.py

--- pages/page2.py
+++ pages/page2.py
@@ -1,10 +1,17 @@
 import streamlit as st
+import pandas as pd
 
 st.title("Page 2")
 
-
-if "Close" in data:
-   st.subheader("Closing Price Over Time")
-   st.line_chart(data['Close'])
+if 'data' in st.session_state:
+   st.write("Statistics of the DataFrame:")
+   st.write(st.session_state.data.describe())
 else:
-   st.warning("Closing price data is not available for this stock.")
+   st.write("No DataFrame found. Please create it in the 'Create DataFrame' page.")
+
+
+#if "Close" in data:
+#   st.subheader("Closing Price Over Time")
+#   st.line_chart(data['Close'])
+#else:
+#   st.warning("Closing price data is not available for this stock.")
