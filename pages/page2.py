0999fccfb38794feacb9ddf58da70aea761900ab
wysocki-bu <wysockip@bu.edu>
2025-03-02 10:33:39 -0500

    Update page2.py

--- pages/page2.py
+++ pages/page2.py
@@ -1,3 +1,10 @@
 import streamlit as st
 
 st.title("Page 2")
+
+
+if "Close" in data:
+   st.subheader("Closing Price Over Time")
+   st.line_chart(data['Close'])
+else:
+   st.warning("Closing price data is not available for this stock.")
