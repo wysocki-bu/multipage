06d066bce78756a0ede5fc2668f0a29a2b47aa0f
wysocki-bu <wysockip@bu.edu>
2025-03-02 11:22:21 -0500

    Update page3.py

--- pages/page3.py
+++ pages/page3.py
@@ -4,7 +4,7 @@ st.title("Page 3")
 
 if 'data' in st.session_state:
    st.write("Closing Price Over Time:")
-   st.line_chart(data['Close'])
+   st.line_chart(data)
 else:
    st.write("No DataFrame found. Please create it in the 'Create DataFrame' page.")
 
