128b74cc9b65e17d3172226185d2eb7ac48d7f58
wysocki-bu <wysockip@bu.edu>
2025-03-01 20:24:24 -0500

    Update page1.py

--- pages/page1.py
+++ pages/page1.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 #import yfinance as yf
 
-st.title("Stock Dashboard")
+st.title("Page 1: Stock Dashboard")
 ticker = st.sidebar.text_input('Ticker')
 start_date = st.sidebar.date_input('Start Date')
 end_date = st.sidebar.date_input('End Date')
