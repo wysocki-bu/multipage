ec4f0630b8a46ec5c3b62d014deabfed85910ce6
wysocki-bu <wysockip@bu.edu>
2025-03-01 20:23:35 -0500

    Update page1.py

--- pages/page1.py
+++ pages/page1.py
@@ -1,3 +1,10 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+#import yfinance as yf
 
-st.title("Page 1")
+st.title("Stock Dashboard")
+ticker = st.sidebar.text_input('Ticker')
+start_date = st.sidebar.date_input('Start Date')
+end_date = st.sidebar.date_input('End Date')
+
