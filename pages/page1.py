4b9e2081f1837e4805059f29eb2b5dc082991e8a
wysocki-bu <wysockip@bu.edu>
2025-03-01 20:43:22 -0500

    Update page1.py

--- pages/page1.py
+++ pages/page1.py
@@ -2,7 +2,11 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+df = pd.read_csv('/data/msft.csv')
+
 st.title("Page 1: Stock Dashboard")
+df
+
 ticker = st.sidebar.text_input('Ticker')
 start_date = st.sidebar.date_input('Start Date')
 end_date = st.sidebar.date_input('End Date')
