c9aaea613aaddd68e35adc6a6631493735bd1a39
wysocki-bu <wysockip@bu.edu>
2025-03-02 20:11:27 -0500

    Update page3.py

--- pages/page3.py
+++ pages/page3.py
@@ -4,7 +4,8 @@ st.title("Page 3: Close Price data")
 
 if 'data' in st.session_state:
    st.write("Statistics of the DataFrame:")
-#   cl_prc=st.session_state.data(MSF.describe())
+   cl_prc=st.session_state.data("Close")
+   st.write(cl_prc)
 else:
    st.write("No stock data found. Please select stock on Main Page.")
 #   st.write("Closing Price Over Time:")
