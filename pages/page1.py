4abc0f8689373fd28b82602d3f1beb7b0cb6a5b4
wysocki-bu <wysockip@bu.edu>
2025-03-02 21:06:48 -0500

    Update page1.py

--- pages/page1.py
+++ pages/page1.py
@@ -5,6 +5,6 @@ import yfinance as yf
 # Page title
 st.title("Page 1: View Stock Data")
 
-st.data_editor(st.session_state.data)
+st.dataframe(st.session_state.data, use_container_width=True)
 
 #st.session_state.data = st.data_editor(st.session_state.data)
