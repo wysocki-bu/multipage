4ff60907f78da81c7b1e1d86c7630be056be56dd
wysocki-bu <wysockip@bu.edu>
2025-03-02 20:08:42 -0500

    Update page3.py

--- pages/page3.py
+++ pages/page3.py
@@ -1,10 +1,14 @@
 import streamlit as st
 
-st.title("Page 3")
+st.title("Page 3: Close Price data")
 
 if 'data' in st.session_state:
-   st.write("Closing Price Over Time:")
-   st.line_chart(data)
+   st.write("Statistics of the DataFrame:")
+#   cl_prc=st.session_state.data(MSF.describe())
+else:
+   st.write("No stock data found. Please select stock on Main Page.")
+#   st.write("Closing Price Over Time:")
+#   st.line_chart(data)
 else:
    st.write("No DataFrame found. Please create it in the 'Create DataFrame' page.")
 
