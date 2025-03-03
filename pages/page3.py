95be25492113ed1382ead93fae9975b9d7016326
wysocki-bu <wysockip@bu.edu>
2025-03-02 20:43:32 -0500

    Update page3.py

--- pages/page3.py
+++ pages/page3.py
@@ -4,7 +4,7 @@ st.title("Page 3: Close Price data")
 
 if 'data' in st.session_state:
    st.write("Variables in DataFrame:")
-   st.write(st.session_state.data)
+   st.write(st.session_state.data['Date'])
 else:
    st.write("No stock data found. Please select stock on Main Page.")
 
