43cef748e8b09c2c9e677a998f6514b7addcacff
wysocki-bu <wysockip@bu.edu>
2025-03-06 11:21:58 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,27 +3,27 @@ import streamlit as st
 # **** Page layout setup ****
 App_page_0 = st.Page(
     "pages/main.py",
-    title="Main Page",
+    title="Click here to select stock",
     default=True
 )
 App_page_1 = st.Page(
     "pages/page1.py",
-    title="Page 1: Stocks"
+    title="Page 1: See Raw Stock Data"
 )
 App_page_2 = st.Page(
     "pages/page2.py",
-    title="Page 2"
+    title="Page 2: See Data Statistics"
 )
 App_page_3 = st.Page(
     "pages/page3.py",
-    title="Page 3"
+    title="Page 3: See Stock Price Graph"
 )
 
 # **** Set up navigation with section headers ****
 pg = st.navigation(
     {
-        "General": [App_page_0],
-        "Specific Pages": [App_page_1, App_page_2, App_page_3],
+        "Start Here:": [App_page_0],
+        "Dashboard Options": [App_page_1, App_page_2, App_page_3],
     }
 )
 
