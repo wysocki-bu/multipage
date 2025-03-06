630ad657ec96f1e9ff6729dbac3fb38eb4f6bebc
wysocki-bu <wysockip@bu.edu>
2025-03-06 14:02:25 -0500

    Update page1.py

--- pages/page1.py
+++ pages/page1.py
@@ -1,10 +1,14 @@
 import streamlit as st
 import pandas as pd
-import yfinance as yf
 
-# Page title
+# Print out the title for Page 1: View stock info dataframe
 st.title("Page 1: View Stock Data")
+
+# Display the Pandas dataframe of the stock data from yfinance
+#  Note: The stock info dataframe is stored in a StreamLit "session state" that allows the data to be shared across
+#        the mutiple pages (ie, main.py, page1.py, page2.py and page4.py)
 
 st.dataframe(st.session_state.data, use_container_width=True)
 
+# The dataframe can also be displayed as an editable interactive dataframe using the StreamLit data_editor function 
 #st.session_state.data = st.data_editor(st.session_state.data)
